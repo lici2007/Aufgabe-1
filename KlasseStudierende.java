e45942053334c792dab94300a4d065221bbcac44
Alicia <budackalicia@gmail.com>
2026-03-24 10:11:31 +0100

    KlasseStudierende_ Aufgabe1

---
+++ KlasseStudierende.java
@@ -0,0 +1,16 @@
+public class KlasseStudierende {
+    private String name;
+    private int matrikelnummer;
+    private String studiengang;
+
+    public studierender(String name, int matrikelnummer, String studiengang){
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    public frageStellen(String frage){
+        return name + matrikelnummer + studiengang + ": " + frage;
+        
+    }
+}
