08d756a137430cfedb5140e7eff95de21b5055cd
Alicia <budackalicia@gmail.com>
2026-03-30 10:55:19 +0200

    Alicia Verbesserung

--- KlasseStudierende.java
+++ KlasseStudierende.java
@@ -3,13 +3,13 @@ public class KlasseStudierende {
     private int matrikelnummer;
     private String studiengang;
 
-    public studierender(String name, int matrikelnummer, String studiengang){
+    public studierender (String name, int matrikelnummer, String studiengang){
         this.name = name;
         this.matrikelnummer = matrikelnummer;
         this.studiengang = studiengang;
     }
 
-    public frageStellen(String frage){
+    public frageStellen (String frage){
         return name + ", " + matrikelnummer + ", " + studiengang + ": " + frage;
 
     }
