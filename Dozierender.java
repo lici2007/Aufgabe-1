5bb3c2613f1ce8137c1d2dadfa0e48aa1938a001
Sophia <sob4113@thi.de>
2026-03-30 11:27:59 +0200

    Ansatz Aufgabe 3

--- Dozierender.java
+++ Dozierender.java
@@ -2,6 +2,8 @@ public class Dozierender {
     private String name; 
     private String fakultaet;
     private String bueronummer;
+    private String sprechstunde;
+    private String feedback;
 
 
 public Dozierender (String name, String fakultaet, String bueronummer) {
@@ -12,4 +14,14 @@ public Dozierender (String name, String fakultaet, String bueronummer) {
 public String frageBeantworten (KlasseStudierender studierender, String antwort){
     return "Antwort von" + name + "an " + studierender.getName() + ": Die Antwort auf Ihre Frage ist: " + antwort;
 }
+public String getSprechstunde () {
+    return sprechstunde; 
 }
+public void setSprechstunde (String sprechstunde) {
+    this.sprechstunde = sprechstunde;
+
+public String feedbackerhalten (KlasseStudierender studierender, String feedback) {
+    return "Feedback von" + studierender.getName() + ": " + feedback;
+}
+
+
