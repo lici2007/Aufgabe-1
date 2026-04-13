7e1126b6b086823e98b5ce43ca00d13aa90051ac
Sophia <sob4113@thi.de>
2026-04-13 19:38:07 +0200

    Verbesserung Praktkiumsstunde

--- Praktikumsstunde.java
+++ Praktikumsstunde.java
@@ -1,20 +1,20 @@
 public class Praktikumsstunde {
-    private String Thema; 
-    private String Datum; 
-    private String Uhrzeit; 
-    private String Raum; 
-    private String Aufgaben; 
+    private String thema; 
+    private String datum; 
+    private String uhrzeit; 
+    private String raum; 
+    private String aufgaben; 
 
-    public Praktikumsstunde (String Thema, String Datum, String Uhrzeit, String Raum, String Aufgaben) {
-        this.Thema = Thema;
-        this.Datum = Datum;
-        this.Uhrzeit = Uhrzeit;
-        this.Raum = Raum;
-        this.Aufgaben = Aufgaben;
+    public Praktikumsstunde (String thema, String datum, String uhrzeit, String raum, String aufgaben) {
+        this.thema = thema;
+        this.datum = datum;
+        this.uhrzeit = uhrzeit;
+        this.raum = raum;
+        this.aufgaben = aufgaben;
     }
 
     public String PraktikumsdetailsAnzeigen () {
-        return "Praktikumsstunde: " + Thema + "\nDatum: " + Datum + "\nUhrzeit: " + Uhrzeit + "\nRaum: " + Raum + "\nAufgaben: " + Aufgaben;
+        return "Praktikumsstunde: " + thema + "\nDatum: " + datum + "\nUhrzeit: " + uhrzeit + "\nRaum: " + raum + "\nAufgaben: " + aufgaben;
     }
 
 }
