a67c8ab17e133b9ebfdf03743f66e2ef50ebdd95
Sophia <sob4113@thi.de>
2026-04-13 20:40:45 +0200

    neue Verbesserung Dozierender

--- Dozierender.java
+++ Dozierender.java
@@ -6,7 +6,7 @@ public class Dozierender {
     private String feedback;
 
 
-    private String feedbackListe = new String();
+    private String[] feedbackListe = new String[0];
 
     public Dozierender (String name, String fakultaet, String bueronummer) {
     this.name = name;
@@ -14,21 +14,30 @@ public class Dozierender {
     this.bueronummer = bueronummer;
 }
 public String frageBeantworten (KlasseStudierende studierende, String antwort){
-    return "Antwort von" + name + "an " + studierende.getName() + ": Die Antwort auf Ihre Frage ist: " + antwort;
+    return "Antwort von " + name + " an " + studierende.getName() + ": Die Antwort auf Ihre Frage ist: " + antwort;
 }
 public String getSprechstunde () {
     return sprechstunde; 
 }
 public void setSprechstunde (String sprechstunde) {
     this.sprechstunde = sprechstunde;
+}
+public void feedbackErhalten (KlasseStudierende studierende, String feedback) {
+    String eintrag = studierende.getName() + ": " + feedback;
 
-public String feedbackErhalten (KlasseStudierende studierende, String feedback) {
-    return "Feedback von" + studierende.getName() + ": " + feedback;
+        String[] neu = new String[feedbackListe.length + 1];
+
+        for (int i = 0; i < feedbackListe.length; i++) {
+            neu[i] = feedbackListe[i];
+        }
+
+        neu[feedbackListe.length] = eintrag;
+        feedbackListe = neu;
 }
 
 public void zeigeFeedback () {
-    for (String feedback : feedbackListe) {
-        System.out.println("Feedback für " + name + ": " + feedback);
+    for (int i = 0; i < feedbackListe.length; i++) {
+            System.out.println(feedbackListe[i]);
     }
 }
 public String getName() {
@@ -39,8 +48,9 @@ public Lehrveranstaltung erstelleLehrveranstaltung (String titel, String[] studi
     return new Lehrveranstaltung(titel, this, studierende);
 }
 
-public void verwalteTeilnehmer (Lehrveranstaltung lehrveranstaltung) {}
-    System.out.println("Teilnehmerliste:");
-    System.out.println(lehrveranstaltung.VeranstaltungsdetailsAnzeigen());
+
+public void verwalteTeilnehmer(Lehrveranstaltung lehrveranstaltung) {
+        System.out.println("Teilnehmerliste:");
+        System.out.println(lehrveranstaltung.VeranstaltungsdetailsAnzeigen());
 }
 }
