40eb54ec9e28fb5ea949b88ca84ce40767c96bde
Sophia <sob4113@thi.de>
2026-04-13 17:40:55 +0200

    Ansatz Klasse Lehrveranstaltung

---
+++ Lehrveranstaltung.java
@@ -0,0 +1,45 @@
+public class Lehrveranstaltung {
+    private String Titel; 
+    private String Dozierende; 
+    private String Studierende; 
+    private String Vorlesungsstunden;
+    private String Praktikumsstunden;
+
+    public Lehrveranstaltung (String Titel, String Dozierende, String Studierende) {
+        this.Titel = Titel;
+        this.Dozierende = Dozierende;
+        this.Studierende = Studierende;
+    }
+
+    public String StudierendeHinzufuegen (KlasseStudierende Studierende) {
+        return "Der Studierende " + Studierende + " wurde zur Lehrveranstaltung " + Titel + " hinzugefügt.";
+    }
+
+    public String StudierendeEntfernen (KlasseStudierende Studierende) {
+        return "Der Studierende " + Studierende + " wurde von der Lehrveranstaltung " + Titel + " entfernt.";
+    }
+    
+    public String VorlesungsstundenHinzufuegen (String Vorlesungsstunden) {
+        this.Vorlesungsstunden = Vorlesungsstunden;
+        return "Die Vorlesungsstunden für die Lehrveranstaltung " + Titel + " wurden auf " + Vorlesungsstunden + " gesetzt.";
+    }
+
+    public String VorlesungsstundenEntfernen () {
+        this.Vorlesungsstunden = null;
+        return "Die Vorlesungsstunden für die Lehrveranstaltung " + Titel + " wurden entfernt.";
+    }
+
+    public String PraktkiumsstundenHinzufuegen (String Praktikumsstunden) {
+        this.Praktikumsstunden = Praktikumsstunden;
+        return "Die Praktikumsstunden für die Lehrveranstaltung " + Titel + " wurden auf " + Praktikumsstunden + " gesetzt.";
+    }
+
+    pubic String PraktikumsstundenEntfernen () {
+        this.Praktikumsstunden = null;
+        return "Die Praktikumsstunden für die Lehrveranstaltung " + Titel + " wurden entfernt.";
+    }
+
+    public String VeranstaltungsdetailsAnzeigen () {
+        return "Lehrveranstaltung: " + Titel + "\nDozierende: " + Dozierende + "\nStudierende: " + Studierende + "\nVorlesungsstunden: " + Vorlesungsstunden + "\nPraktikumsstunden: " + Praktikumsstunden;
+    }
+}
